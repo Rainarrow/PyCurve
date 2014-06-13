83eac83623f99d9743ab405bd947efe28723faea
Rainarrow <rainarrow@gmail.com>
2014-06-12 18:11:30 -0700

    Solving linear system

--- MAT500.py
+++ MAT500.py
@@ -28,6 +28,25 @@ class Point(object):
     def __sub__(self, other):
         return Point(self.x-other.x, self.y-other.y)
 
+def gaussianElim(m):
+    for col in range(len(m[0])):
+        for row in range(col+1, len(m)):
+            r = [(rowValue * (-(m[row][col] / m[col][col]))) for rowValue in m[col]]
+            m[row] = [sum(pair) for pair in zip(m[row], r)]
+
+    result = []
+    m.reverse()
+    for sol in range(len(m)):
+        if sol == 0:
+            result.append(m[sol][-1] / m[sol][-2])
+        else:
+            inner = 0
+            for x in range(sol):
+                inner += (ans[x]*m[sol][-2-x])
+            result.append((m[sol][-1]-inner)/m[sol][-sol-2]    
+    result.reverse()
+    return result
+
 def divDiff(points):
     a = []      # Coefficients
     m = len(points)
